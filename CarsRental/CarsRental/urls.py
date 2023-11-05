0709a8708bb59417f5001fa9ed0dc444493c194f
MaramAlasmari <147453012+MaramAlasmari@users.noreply.github.com>
2023-11-05 23:11:17 +0300

    done

--- CarsRental/CarsRental/urls.py
+++ CarsRental/CarsRental/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from cars_rental import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("cars_rental.urls")),
-    path("", include("about.urls")),
+    path("about", include("about.urls")),
 
 
 ]
