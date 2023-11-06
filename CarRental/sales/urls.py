db2df4ac0e175223f21e26a1ddefdf747c914f3e
khamis_dossary <khamis.dossary@gmail.com>
2023-11-06 13:51:10 +0300

    done

--- CarRental/sales/urls.py
+++ CarRental/sales/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 app_name = "sales"
 urlpatterns = [
-    path("", views.index_view, name="index_view"),
-    path("info/", views.info_view, name="info_view")
+    path("sales/home/", views.index_view, name="home_page"),
+    path("sales/info/", views.info_view, name="info_view")
 ]
--- CarRental/sales/views.py
+++ CarRental/sales/views.py
@@ -4,14 +4,7 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 def index_view(request:HttpRequest):
-    content = """Hello World, This is my new HOME for Car Rentals Website! 
-            We're excited to welcome you here."""
-    return HttpResponse(content)
+    return render(request,'sales/salesPage.html')
 
 def info_view(request:HttpRequest):
-    content = """Car rentals provide a convenient and flexible transportation solution for individuals and travelers. 
-    Whether for business trips or leisure travel, car rental services offer a wide range of vehicles to choose from, allowing customers to select the most suitable option for their needs. 
-    These services often include daily, weekly, or monthly rental options, making it easy for people to access a vehicle without the long-term commitment of ownership. 
-    Car rentals are available at airports, city centers, and other convenient locations, making it easy to pick up and drop off vehicles. 
-    This flexibility has made car rentals a popular choice for those looking for temporary transportation solutions."""
-    return HttpResponse(content)
+       return render(request,'sales/salesInfo.html')
