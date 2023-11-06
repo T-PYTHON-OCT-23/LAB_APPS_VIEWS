9443b24fba0bd868ce79dccf2c64331564e04259
Bader alotaibi <baderalotaibi3@gmail.com>
2023-11-06 12:02:49 +0300

    html pages

--- car_rental/rental/views.py
+++ car_rental/rental/views.py
@@ -4,24 +4,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello World, This is my new HOME for Car Rentals Website! We're excited to welcome you here.")
-
+    return render(request,'main/main.html')
 
 
 def info(request):
-    content='''
     
-    <!DOCTYPE html>
-<html>
-<head>
-    <title>About Cars</title>
-</head>
-<body>
-    <p>When it comes to automobiles, cars have revolutionized the way we live and travel. They offer us the freedom to explore the world, commute to work, and go on exciting road trips. Cars come in various shapes, sizes, and styles, catering to diverse needs and preferences. Whether you're interested in eco-friendly electric cars, powerful sports cars, or practical family vehicles, there's a car for everyone. With advanced technologies and safety features, modern cars make our journeys more comfortable and secure. The automotive industry continues to innovate, making cars an integral part of our daily lives.</p>
-</body>
-</html>
-
-    
-    
-    '''
-    return HttpResponse(content)
+    return render(request,'main/info.html')
