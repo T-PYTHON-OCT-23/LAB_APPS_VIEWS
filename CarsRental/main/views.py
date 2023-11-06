2cee101a9598863da38a4e707bae90e0c16094ac
alya <alyamem80@gmail.com>
2023-11-06 12:02:44 +0300

    done

--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -15,8 +15,8 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 #view in django
 def index_view(request:HttpRequest):
-    content = "Hello World, This is my new HOME for Car Rentals Website ! we're excited to welcome you here."
-    return HttpResponse(content)
+    
+    return render(request, 'main/home.html')
 
 def about_view(request:HttpRequest):
     content = "<h1 style='color:magenta;'>A simple paragraph about Car Rentals </h1>   "
