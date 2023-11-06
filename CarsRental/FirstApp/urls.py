fac1e8fa1725ec3dfa97bd66ec30a3f67c246dc2
Sufana_Aljawi <Sufanaaljawi@gmail.com>
2023-11-06 16:19:13 +0300

    Lab-templates-done

--- CarsRental/FirstApp/urls.py
+++ CarsRental/FirstApp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+from django.urls import include
 from . import views
 app_name = "FirstApp"
 
 urlpatterns = [
-    path("", views.page_view, name="page_view"),
-    path("info/", views.page_info, name="page_info")
+    path("", views.page_view, name="page_view")
 ]
--- CarsRental/FirstApp/views.py
+++ CarsRental/FirstApp/views.py
@@ -4,12 +4,9 @@ from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 def page_view(request : HttpRequest):
-    content = "Hello World, This is my new HOME for Car Rentals Website ! we're excited to welcome you here."
-    return HttpResponse(content)
 
-def page_info(request : HttpRequest):
-    content = "Car rental is a service that allows people to rent a car for a short period of time. It is a popular option for people who are traveling, need a car for a special occasion, or whose car is in the shop. Car rental companies offer a wide variety of cars to choose from, including sedans, SUVs, minivans, and trucks."
-    return HttpResponse(content)
+    return render(request,'FirstApp/home.html')
+
 
 
 # Create your views here.
