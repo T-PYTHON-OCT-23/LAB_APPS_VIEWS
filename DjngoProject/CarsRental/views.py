5e060286d7d988dd7357fcb5631f9af18a710c68
AlbandariAljabri <Albandarialjabri@gmail.com>
2023-11-06 12:48:50 +0300

    done

--- DjngoProject/CarsRental/views.py
+++ DjngoProject/CarsRental/views.py
@@ -4,10 +4,9 @@ from django.http import HttpRequest , HttpResponse
 
 # Create your views here.
 def index_car_view(request: HttpRequest):
-    phrase="Hello World, This is my new HOME for Car Rentals Website! we're excited to welcome you here."
-    return HttpResponse(phrase)
+    return render(request, 'main/index_car_view.html')
+
 
 # Create your views here.
 def index_car_view2(request: HttpRequest):
-    phrase="<p style= 'color:white;  background-color: black;'>Car rentals are very popular today which means that rental agencies are all looking for ways to get your custom. To do this they are offering highly competitive prices and all good agencies will have a wide choice of cars. A good agency will recognize that everyone has their own car of preference and should try to provide cars that fit the needs of their customers.</p>"
-    return HttpResponse(phrase)
+    return render(request, 'main/index_car_view2.html')
