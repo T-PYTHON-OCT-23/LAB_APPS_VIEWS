51e247003f7bf29c4b5e16abea48381e8d6773b0
m7mdhusinn <m7md.husinn@gmail.com>
2023-11-06 12:07:46 +0300

    templates is done

--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
 # Create your views here.
 def car_home(request:HttpRequest):
-    return HttpResponse("Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here.")
+    return render(request,"main/home.html")
 
 def car_info(request:HttpRequest):
-    return HttpResponse("A simple paragraph about Car Rentals. ")
+    return render(request,"main/about.html")
