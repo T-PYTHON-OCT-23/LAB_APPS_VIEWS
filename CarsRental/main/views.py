b387612f6d18e27ea19435946630f2df955e8127
alya <alyamem80@gmail.com>
2023-11-06 06:07:20 +0300

    done

---
+++ CarsRental/main/views.py
@@ -0,0 +1,23 @@
+
+# Create your views here.
+'''from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+# Create your views here.
+def page_view(request : HttpRequest):
+ content = "Hello World, This is my new HOME for Car Rentals Website ! we're excited to welcome you here."
+ return HttpResponse(content)
+def page2_view(request : HttpRequest):
+ content = "A simple paragraph about Car Rentals."
+ return HttpResponse(content)'''
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+# Create your views here.
+#view in django
+def index_view(request:HttpRequest):
+    content = "Hello World, This is my new HOME for Car Rentals Website ! we're excited to welcome you here."
+    return HttpResponse(content)
+
+def about_view(request:HttpRequest):
+    content = "<h1 style='color:magenta;'>A simple paragraph about Car Rentals </h1>   "
+    return HttpResponse(content)
